4a8bc037a3aafabb8f901cb846edd1d276130ee3
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-11-27 22:16:47 +0800

    Heroku support

--- server.js
+++ server.js
@@ -22,6 +22,10 @@ const forceSSL = function() {
 // middleware
 app.use(forceSSL());
 
+// Run the app by serving the static files
+// in the dist directory
+app.use(express.static(__dirname + '/dist'));
+
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
@@ -29,10 +33,6 @@ app.get('/*', function(req, res) {
   res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
-
-// Run the app by serving the static files
-// in the dist directory
-app.use(express.static(__dirname + '/dist'));
 // Start the app by listening on the default
 // Heroku port
 app.listen(process.env.PORT || 8080);
