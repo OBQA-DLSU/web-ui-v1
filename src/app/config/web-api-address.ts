6129ec7d1b6ab23889a8f18c82213c6bb8d80455
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-12-01 00:27:53 +0800

    fix

--- src/app/config/web-api-address.ts
+++ src/app/config/web-api-address.ts
@@ -1,10 +1,1 @@
-export const WEB_API_URL = (): string => {
-  switch (process.env.NODE_ENV) {
-    case 'staging':
-      return '';
-    case 'prod':
-      return '';
-    default:
-      return '127.0.0.1';
-  }
-}
+export const WEB_API_URL: string = '127.0.0.1';
