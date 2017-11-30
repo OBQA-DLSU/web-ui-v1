6d8dcebdeeba1dee8fda3f626e5c0d0b5dce2f4c
johnhigginsavila <johnhiggins.avila@gmail.com>
2017-12-01 03:24:58 +0800

    added redux

--- src/app/config/web-api-address.ts
+++ src/app/config/web-api-address.ts
@@ -1,11 +1,1 @@
-const env: string = 'dev';
-export const WEB_API_URL = (): string => {
-  switch (env) {
-    case 'staging':
-      return '';
-    case 'prod':
-      return '';
-    default:
-      return '127.0.0.1';
-  }
-}
+export const WEB_API_URL = '127.0.0.1';
